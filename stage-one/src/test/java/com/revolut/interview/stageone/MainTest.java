1cc28235d7fc31db6ecb68a1d9dfb6d62d46cd68
Robert Grodecki <rgrodecki@tvcoins.com>
2024-05-28 08:50:26 +0200

    Init maven project with one blank module

---
+++ stage-one/src/test/java/com/revolut/interview/stageone/MainTest.java
@@ -0,0 +1,23 @@
+package com.revolut.interview.stageone;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+  @BeforeEach
+  void setUp() {
+  }
+
+  @AfterEach
+  void tearDown() {
+  }
+
+  @Test
+  void test() {
+    assertThat(true).isTrue();
+  }
+}
